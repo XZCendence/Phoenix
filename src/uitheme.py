91f8abb3ba82c4b0b0c1f41e576c2b8adbd491f5
XZCendence <44044176+XZCendence@users.noreply.github.com>
2022-10-06 23:53:31 -0400

    Add tab theme color to base UI theme

--- src/uitheme.py
+++ src/uitheme.py
@@ -9,13 +9,14 @@ baseThemeColor=(200, 0, 255)    #Purple
 
 #Set up a global theme using the base color
 def setBaseColorTheme():
-    with dpg.theme() as global_theme:
+    with dpg.theme() as theme_monochrome:
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_color(dpg.mvThemeCol_Border, baseThemeColor, category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_TitleBg, (baseThemeColor[0]*0.25, baseThemeColor[1]*0.25, baseThemeColor[2]*0.25), category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_TitleBgActive, baseThemeColor, category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_TabHovered, baseThemeColor, category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_TabActive, baseThemeColor, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_color(dpg.mvThemeCol_Tab, baseThemeColor + (100, 0), category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_Button, baseThemeColor + (25,0), category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, baseThemeColor + (100,0), category=dpg.mvThemeCat_Core)
             dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, baseThemeColor , category=dpg.mvThemeCat_Core)
@@ -29,4 +30,4 @@ def setBaseColorTheme():
             dpg.add_theme_color(dpg.mvThemeCol_PopupBg,(baseThemeColor[0]*0.25, baseThemeColor[1]*0.25, baseThemeColor[2]*0.25) + (230,0) , category=dpg.mvThemeCat_Core)
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
             dpg.add_theme_style(dpg.mvStyleVar_FrameBorderSize, 1, category=dpg.mvThemeCat_Core)
-    dpg.bind_theme(global_theme)
+    dpg.bind_theme(theme_monochrome)
